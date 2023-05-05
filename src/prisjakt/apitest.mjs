340c166055683459d1313589e8947a5824563190
Bård Ingebricson Røtzer <bard.rotzer@finn.no>
2023-05-05 09:21:23 +0200

    update the prisjakt thingy

--- src/prisjakt/apitest.mjs
+++ src/prisjakt/apitest.mjs
@@ -7,8 +7,24 @@ const blacklist = ['Fyndiq', 'Billigteknik']
 
 const base_api_url = 'https://api.schibsted.com/prisjakt/partner-search/products/'
 const market = 'se'
-const clientId = '25bc7a461420417985b7a15d796057d9'
+const CLIENT_ID = '25bc7a461420417985b7a15d796057d9'
+const CLIENT_SECRET = 'b89382403B344BAD983527e418e2D553'
 // const token = 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjlrZi9aakFSWVdOeWxnMUVodE9MVHdrUWVlWT0ifQ.eyJhdWQiOiJodHRwczovL2FwaS5zY2hpYnN0ZWQuY29tL3ByaXNqYWt0L3BhcnRuZXItc2VhcmNoIiwiaXNzIjoiaHR0cHM6Ly9hcGkuc2NoaWJzdGVkLmNvbS9wcmlzamFrdC9iMmItYXV0aCIsInN1YiI6IjI1YmM3YTQ2MTQyMDQxNzk4NWI3YTE1ZDc5NjA1N2Q5IiwiaWF0IjoxNjc2NDY3MTA3LCJleHAiOjE2NzY0Njc3MDd9.Mj0U_K3Gyqf8TY0hdhOd-UK6APKeTG6XQVF0-Jk6B9IjMK-vd1t3hZG0ZKFzRLgjvpLDllZ38MjJFRs7RJ53PWRn5fpONkxOnS2fXkX3wD10ZG0SEWb3m6te1KWcw4jBUTbtq0Uf3yo8vTZEJqN-LjYzdL0H0H6FHojlcVRtdSFa9ntaM3f0mOn8vEy0FgGFKqbElvhs9U9Fcb7qq3s_NKawrRkYVlfsqehPgBvHgIvuUBvia57ZPqA5GOLfJBe20NH6begyxOJp_Ivir3fzrBKaa7y5yldHjtjAwZNxHXogqegJCvW-x3q3A71UlQrGGMmoj_WJzF1uJ8B1DrIsfA'
+
+
+const createCsvData = (data) => {
+  if (data.offers && data.offers.length) {
+    console.log(`${data.productId}, ${data.productName}, , , `)
+    let mean = data.offers.reduce((acc, curr) => acc + curr.price.value, 0) / data.offers.length
+    console.log(`, Shop, Price, Mean ~ (${Math.round(mean)}), Diff from mean (%) `)
+    data.offers.forEach(o => {
+      console.log(`, ${o.shop.name}, ${o.price.value},, ${Math.round(((mean-o.price.value)/o.price.value)*100)} `)
+
+    })
+    console.log(`, , , , `)
+  }
+}
+
 
 /**
  * returns the lowest price after cleaning up the suppliers that are providing used phones
@@ -34,8 +50,8 @@ const getToken = async() => {
   const postUrl = 'https://api.schibsted.com/prisjakt/partner-search/token'
   const { data } = await axios.post(postUrl, {
       'grant_type': 'client_credentials',
-      'client_id': '25bc7a461420417985b7a15d796057d9',
-      'client_secret': 'b89382403B344BAD983527e418e2D553'
+      'client_id': CLIENT_ID,
+      'client_secret': CLIENT_SECRET,
     }, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
@@ -45,7 +61,6 @@ const getToken = async() => {
   return data.access_token
   }
 
-
 const lookupPhone = (pid, accessToken) => {
 
   const url = `${base_api_url}${pid}?market=${market}`
@@ -54,60 +69,104 @@ const lookupPhone = (pid, accessToken) => {
     axios.get(url, {
        headers: {
          'Authorization': `Bearer ${accessToken}`,
-         'client-id': clientId
+         'client-id': CLIENT_ID
        }
      }).then(function({status, data}) {
-        console.log(data)
-       console.log(data.offers)
+        console.log('data',data)
+        console.log('status', status)
+
+        if (data.success) {
+          data.data.offers.forEach(d => {
+            console.log(d.shop.name)
+            console.log(d.price.value)
+            console.log(d.alternativePrices)
+          })
+          console.log('offers', data.data.offers)
+        }
        const stat = status === 200 ? 'OK' : 'Error'
-       const price = getLowestPrice(data.offers)
-        resolve(price)       
+      //  const price = getLowestPrice(data.offers)
+
+      // createCsvData(data)
+      // resolve(data.offers.length)       
      })
-     .catch(function (error) {
-       const stat = error.response.status === 401 ? 'Missing' : 'Error'
-       console.log('id: ', pid, stat, error.response.status)
+     .catch((error) => {
+      console.log('error', error)
+      //  const stat = error.response.status === 401 ? 'Missing' : 'Error'
+      //  console.log('id: ', pid, stat, error.response.status)
        reject(0)
      })
   })
 }
 
 const test = async () => {
-  const json = await readjson('./prisjakt-nybrukt.json')
-  const keys = Object.keys(json)
+  const json = await readjson('./prisjakt-lookup.json')
   const accessToken = await getToken()
   const priceData = []
+  // 5338238
 
 
-  await lookupPhone("5183925", accessToken).then((price) => {
-    console.log(price)
-  }).catch(e => {
-    console.error(e)
-  })
+  // const errlist = [5187546, 5594641, 5338238, 5594642, 5595710, 5595712, 5595711, 5683804, 5855621, 5855617, 5855648, 5855649, 5855650, 5855644, 5851667, 5855622, 5855618, 5855652, 5855651, 5956327, 5956321, 5956325, 5956320, 5956311, 6922579, 5057278, 5838252]
+  // for(let i = 0; i < errlist.length; i += 1) {
+  //   const pid = errlist[i]
+    
+    // await lookupPhone('5172022', accessToken)
+    // .then((price) => {
+    //   console.log(5172022, price)
+    // }).catch(e => {
+    //   console.error(5172022, 'err',e)
+    // })
+  // }
+
+  // await lookupPhone("5594641", accessToken).then((price) => {
+  //   console.log(price)
+  // }).catch(e => {
+  //   console.error(e)
+  // })
 
 
   // console.log("name", ",prisjakt_price", ",prisjakt_id", ",model")
+  const start = 10 // 0
+  const end =  12// json.length
+  for(let i = start; i < end; i += 1) {
 
-  // const numLoops = keys.length
-  // for(let i = 0; i < numLoops; i += 1) {
-  // const start = 21
-  // const numLoops = start + 1
-  // for(let i = start; i < numLoops; i += 1) {
-  //   const pid = json[keys[i]].pid
+
+    const pid = json[i].pid
     
-  //   await lookupPhone(pid).then((price) => {
-  //     priceData.push({
-  //       name: data.productName,
-  //       id: json[keys[i]].id,
-  //       storage: json[keys[i]].storage,
-  //       prisjaktid: json[keys[i]].pid,
-  //       price: price
-  //     })
-  //   })
-  // }
+    await lookupPhone(pid, accessToken)
+    .then((price) => {
+      priceData.push({
+        name: json[i].name,
+        id: json[i].id,
+        storage: json[i].storage,
+        prisjaktid: json[i].pid,
+        price: price
+      })
+    })
+    .catch(e => {
+      // console.log('error in', json[i].pid,'----', json[i].name)
+      priceData.push({
+        name: json[i].name,
+        id: json[i].id,
+        storage: json[i].storage,
+        prisjaktid: json[i].pid,
+        price: 0
+      })
+    })
+  }
 
   // writejson(`./prisjakt-from-api-${market}.json`, JSON.stringify(priceData))
 }
 
+const calc = () => {
+  
+  const nums = [2026,2299,3566,3797,3799,5870]
+  let sum = 0
+  nums.forEach(n => {
+    sum += n
+  })
+  console.log(sum, (Math.round(sum/6)))
+}
+// calc()
 test()
 // getToken()
 
--- src/prisjakt/create.mjs
+++ src/prisjakt/create.mjs
@@ -13,13 +13,25 @@ const LOOKUP_FILE = './prisjakt-lookup.json'
 const output = async () => {
   const data = await read(DATA_FILE)
   const json = await readjson(LOOKUP_FILE)
-  
+  const comparejson = await readjson('./prisjakt-pricelist-se-old.json')
   const models = []
+
+  // iterate over all the phone variants defined in the lookup file
   json.forEach(j => {
-    const d = data.find(d => d.name === j.name)
+    // get the corresponding phone in the prisjakt data file
+    const prisjaktphone = data.find(d => d.name === j.name)
     let price = 0
-    if (d) {
-      price = Math.round(d.price)
+    if (prisjaktphone) {
+      const compare = comparejson.find(d => d.id === j.id && d.storage === j.storage && d.color === j.color)
+      price = Math.round(prisjaktphone.price)
+      if (compare && compare.price && price) {
+        if (price / compare.price < 0.7) {
+          console.log(j.pid, '--', j.name, price, compare.price, 'same:', price / compare.price)
+          price = 0
+        } else if (price / compare.price > 1.1) {
+          console.log(j.pid, '--', j.name, price, compare.price, 'same:', price / compare.price)
+        }
+      } 
     }
     //
     models.push({
